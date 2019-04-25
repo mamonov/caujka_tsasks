0f6511405a87540e3dac983e21056ce3a857f6a4
ymamonov <yuriy.mamonov@edvantis.com>
2019-04-25 18:36:47 +0300

    #1

--- CSV_to_db/csv_to_json.py
+++ CSV_to_db/csv_to_json.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import datetime
 
 csvfilepath = "statistic_data.csv"
 jsonfilepath = 'amenities.json'
@@ -9,8 +10,11 @@ x = 0
 with open(csvfilepath) as csvfile:
     csvreader = csv.reader(csvfile)
     for csvrow in csvreader:
-        print(csvrow)
-        data.append(csvrow)
+        data.append(
+            "INSERT INTO `counters`(`date`, `gas`, `water`, `electricity_day`, `electricity_night`, `electricity_overall`) VALUES(" + "'" +
+            datetime.datetime.strptime(csvrow[0], "%d.%m.%Y").strftime("%Y-%m-%d") + "', " + "'" + csvrow[
+                1] + "', " + "'" + csvrow[2] + "', " + "'" + csvrow[3] + "', " + "'" + csvrow[
+                4] + "', " + "'" + csvrow[5] + "');")
 
 with open(jsonfilepath, "w") as jsonfile:
     jsonfile.write(json.dumps(data, indent=4))
